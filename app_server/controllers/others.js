cf05b628b457c543b5dc3964b28e93ec5203198a
vallisneria <moogi3469@gmail.com>
2020-09-19 23:24:58 +0900

    실습과제 6-2

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,8 @@
 module.exports.about = function (req, res) {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+        title: 'About',
+        content: 'Loc8r was created to help people find places to sit down and get a bit of work donw.<br/><br/>'
+            + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            + 'Pellentesque ornare luctus enim, sit amet tristique lectus maximus a. Aliquam erat volutpat.'
+    });
 }
