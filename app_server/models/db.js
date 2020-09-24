251279e06ccb8eaf7d05f2d45111c98688f58b5c
vallisneria <moogi3469@gmail.com>
2020-09-24 20:45:03 +0900

    실습과제7

---
+++ app_server/models/db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const dbURI = 'mongodb://localhost/loc8r';
+mongoose.connect(dbURI, { useNewUrlParser: true });
+
+// 데이터베이스 연결
+mongoose.connection.on('connected', function () {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', function (error) {
+    console.log(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log(`Mongoose disconnected`);
+});
+
+process.once('SIGUSR2', function () {
+    gracefulShutdonw('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// 데이터베이스 Closing
+process.on('SIGINT', function () {
+    gracefulShutdonw('app termination', function () {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function () {
+    gracefulShutdonw('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
+
+
+function gracefulShutdonw(msg, callback) {
+    mongoose.connection.close(function () {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+}
+
+require('./locations');
