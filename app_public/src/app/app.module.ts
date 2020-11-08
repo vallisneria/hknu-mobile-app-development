e609334e3814f48d717fc6c2b6a2d11f8f33877e
vallisneria <moogi3469@gmail.com>
2020-11-08 19:48:36 +0900

    실습과제 14-2
    - 장소 속성이 바뀌거나 여러개여도 표시할 수 있도록 작성

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HomeListComponent } from './home-list/home-list.component'
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe'
 
 @NgModule({
   declarations: [
     HomeListComponent,
+    DistancePipe,
   ],
   imports: [
     BrowserModule
--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[]
+}
+
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
   styleUrls: ['./home-list.component.css']
 })
-export class HomeListComponent implements OnInit {
 
+export class HomeListComponent implements OnInit {
   constructor() { }
+
+  locations: Location[] = [{
+    _id: "5f6f424011dd734b3b63aad3",
+    name: "Sushi",
+    distance: 356321651,
+    address: "경기도 김포시 김포2동 385-9",
+    rating: 3,
+    facilities: ["Premium wifi", "Water"]
+  }, {
+    _id: "5f6f424011dd734b3b63aad3",
+    name: "Starcups",
+    distance: 416542,
+    address: "경기도 김포시 김포한강5로 385-9",
+    rating: 4,
+    facilities: ["Hot Drinks", "Wi-Fi"]
+  }]
 
   ngOnInit(): void {
   }
