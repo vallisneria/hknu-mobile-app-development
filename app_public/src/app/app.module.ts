0e477aef9950aeeb6d7feea521d8cc1c3794a30f
vallisneria <moogi3469@gmail.com>
2020-11-15 01:16:50 +0900

    실습과제 16

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe'
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from "@angular/router";
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     HomeListComponent,
     DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent },
+      { path: 'about', component: AboutComponent },
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
