5826c8a6f62bc9e7110c3ffc78b8b19248ec1ea0
vallisneria <moogi3469@gmail.com>
2020-11-23 18:48:58 +0900

    rating-star 구현

--- app_public/src/app/rating-stars/rating-stars.component.ts
+++ app_public/src/app/rating-stars/rating-stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rating-stars',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating-stars.component.css']
 })
 export class RatingStarsComponent implements OnInit {
+
+  @Input() rating: number;
 
   constructor() { }
 
