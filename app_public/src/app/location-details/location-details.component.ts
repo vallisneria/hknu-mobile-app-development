2d407c2bfd786e01c71604ebbfbdb7dadbebd3ee
vallisneria <moogi3469@gmail.com>
2020-11-23 18:11:52 +0900

    실습과제 19-4
    - 리뷰 작성할 수 있도록 변경

--- app_public/src/app/location-details/location-details.component.ts
+++ app_public/src/app/location-details/location-details.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '../home-list/home-list.component';
 import { Component, OnInit, Input } from '@angular/core';
+import { Loc8rDataService } from "../loc8r-data.service";
 
 @Component({
   selector: 'app-location-details',
@@ -20,7 +21,7 @@ export class LocationDetailsComponent implements OnInit {
 
   public formVisible: boolean = false;
 
-  constructor() { }
+  constructor(private loc8rDataService: Loc8rDataService) { }
 
   ngOnInit(): void {
   }
@@ -31,14 +32,28 @@ export class LocationDetailsComponent implements OnInit {
     return Boolean(this.newReview.author && this.newReview.rating && this.newReview.reviewText);
   }
 
+  private resetAndHideReviewForm(): void {
+    this.formVisible = false;
+    this.newReview.author = "";
+    this.newReview.rating = 5;
+    this.newReview.reviewText = "";
+  }
+
   public onReviewSubmit(): void {
     this.formError = '';
     if (this.formIsValid()) {
       console.log(this.newReview);
+      this.loc8rDataService.addReviewByLocationId(this.location._id, this.newReview)
+        .then(review => {
+          console.log('Review Saved', review)
+          let reviews = this.location.reviews.slice(0);
+          reviews.unshift(review);
+          this.resetAndHideReviewForm();
+        });
+
     } else {
       this.formError = 'All fields required, please try again.'
     }
-    console.log(this.newReview);
   }
 
 }
